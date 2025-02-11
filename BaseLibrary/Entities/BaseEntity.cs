f4c4bf8e94a4d0de6d46227fa57b33d78e2b0c92
Thomas <thomasfurniere@hotmail.com>
2025-02-11 16:57:56 +0100

    Updating all packages, classes, dbsets, migration, updating database

---
+++ BaseLibrary/Entities/BaseEntity.cs
@@ -0,0 +1,18 @@
+﻿
+
+using System.Text.Json.Serialization;
+
+namespace BaseLibrary.Entities
+{
+    public class BaseEntity
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+
+        //Relation: One to Many
+        [JsonIgnore]
+        public List<Employee>? Employees { get; set; }
+
+
+    }
+}
--- Server/Program.cs
+++ Server/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using ServerLibrary.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +10,13 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+//starting
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection") ??
+        throw new InvalidOperationException("Sorry, your connection failed"));
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -15,6 +25,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
 
 app.UseHttpsRedirection();
 
