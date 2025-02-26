6f20e6573c31e8b56add58410786e2755cb2445d
Thomas <thomasfurniere@hotmail.com>
2025-02-26 23:39:31 +0100

    1.40.36GetHttpClientInHelpers

---
+++ CliëntLibrary/Services/Contracts/IUserAccountService.cs
@@ -0,0 +1,14 @@
+﻿using BaseLibrary.DTOs;
+using BaseLibrary.Responses;
+
+
+namespace CliëntLibrary.Services.Contracts
+{
+    public interface IUserAccountService
+    {
+        Task<GeneralResponse> CreateAsync(Register user);
+        Task<LoginResponse> SignInAsync(Login user);
+        Task<LoginResponse> RefreshTokenAsync(RefreshToken token);
+        Task<WeatherForecast[]> GetWeatherForecast();
+    }
+}
---
+++ CliëntLibrary/Services/Implementations/UserAccountService.cs
@@ -0,0 +1,30 @@
+﻿using BaseLibrary.DTOs;
+using BaseLibrary.Responses;
+using CliëntLibrary.Services.Contracts;
+
+namespace CliëntLibrary.Services.Implementations
+{
+    public class UserAccountService : IUserAccountService
+    {
+        public Task<GeneralResponse> CreateAsync(Register user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<LoginResponse> SignInAsync(Login user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<LoginResponse> RefreshTokenAsync(RefreshToken token)
+        {
+            throw new NotImplementedException();
+        }
+
+        
+        public Task<WeatherForecast[]> GetWeatherForecast()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
